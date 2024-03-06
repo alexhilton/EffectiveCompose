c9179e802746ac0ab875be6c7162c860f54d13ce
Alex Hilton <dragonhilton@gmail.com>
2024-03-06 14:46:41 +0800

    Learn how to animate visibility.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

---
+++ app/src/main/java/net/toughcoder/effectivecompose/Animations.kt
@@ -0,0 +1,100 @@
+package net.toughcoder.effectivecompose
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AnimateVisibility() {
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        WithAnimatedVisibility()
+
+        WithAlphaAsState()
+    }
+}
+
+/*
+ * This won't work when first compose, i.e. when launching.
+ * It only works after views are composed. When you toggle
+ * the button after everything shown, for instance.
+ * We can see that when animating exit, the animated
+ * composables are removed from layouts. This is the biggest
+ * difference from animate alpha as state.
+ */
+@Composable
+private fun WithAnimatedVisibility() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        var visible by remember { mutableStateOf(true) }
+
+        AnimatedVisibility(visible) {
+            Box(
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color.Green)
+            )
+        }
+
+        Button(
+            onClick = { visible = !visible }
+        ) {
+            Text("(AnimatedVisibility) Show/Hide")
+        }
+    }
+}
+
+@Composable
+private fun WithAlphaAsState() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        var visible by remember { mutableStateOf(true) }
+        val alphaState by animateFloatAsState(
+            targetValue = if (visible) 1f else 0f,
+            label = "alpha as state"
+        )
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .graphicsLayer { alpha = alphaState }
+                .clip(RoundedCornerShape(8.dp))
+                .background(Color.Green)
+        )
+
+        Button(
+            onClick = { visible = !visible }
+        ) {
+            Text("(Alpha as state) Show/Hide")
+        }
+    }
+}
--- app/src/main/java/net/toughcoder/effectivecompose/MainActivity.kt
+++ app/src/main/java/net/toughcoder/effectivecompose/MainActivity.kt
@@ -3,13 +3,18 @@ package net.toughcoder.effectivecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.toughcoder.effectivecompose.ui.theme.EffectiveComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Greeting("Android")
+
+                        Spacer(Modifier.height(16.dp))
+
+                        AnimateVisibility()
+                    }
                 }
             }
         }
